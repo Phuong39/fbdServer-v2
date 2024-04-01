6cf769178911c38e19aa92d4fbcce455362ef7a9
James Smith <jismith1989@googlemail.com>
2024-04-01 16:33:14 +0100

    Use globalFilepath in template package

--- template/template.go
+++ template/template.go
@@ -2,14 +2,24 @@ package template
 
 import (
 	"github.com/kataras/blocks"
+	globalFilepath "github.com/theTardigrade/golang-globalFilepath"
 )
 
 var (
 	Views *blocks.Blocks
 )
 
+const (
+	rootDirPath           = "view"
+	layoutDirPathRelative = "layout"
+)
+
 func init() {
-	Views = blocks.New("view").LayoutDir("layout")
+	globalFilepath.Init("..")
+
+	rootDirPathAbs := globalFilepath.Join(rootDirPath)
+
+	Views = blocks.New(rootDirPathAbs).LayoutDir(layoutDirPathRelative)
 
 	err := Views.Load()
 	if err != nil {
