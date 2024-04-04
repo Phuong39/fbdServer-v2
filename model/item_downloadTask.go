43ab44ca4829407e6e5c96e18f82376e412f7123
James Smith <jismith1989@googlemail.com>
2024-04-05 00:24:36 +0100

    Slightly improve item hashedGUID algorithm

--- model/item_downloadTask.go
+++ model/item_downloadTask.go
@@ -241,13 +241,13 @@ func itemParseFromRemoteStore(rawItem *gofeed.Item, storeName string) (err error
 	var hashedGUID string
 
 	{
-		hashedGUID = hash.Uint128String(guid).Text(16)
+		hashedGUID = strconv.FormatUint(uint64(hash.Uint32String(guid)), 16)
 
 		for i := len(hashedGUID); i < 4; i++ {
 			hashedGUID = "0" + hashedGUID
 		}
 
-		hashedGUID = hashedGUID[:4]
+		hashedGUID = hashedGUID[len(hashedGUID)-4:]
 
 		hashedGUID += strconv.FormatUint(hash.Uint64String(guid), 16)
 	}
